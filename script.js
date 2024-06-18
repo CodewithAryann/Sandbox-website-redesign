0c1961ac704d82668e0832d628ba254b0407041d
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-06-18 18:24:01 +0400

    6/18/2024

--- script.js
+++ script.js
@@ -68,7 +68,229 @@ function loaderAnimation() {
         loader.style.top = "-100%"
     }, 4200)
 }
+function sandboximg() {
+    gsap.to("#img1", {
+        scrollTrigger: {
+            trigger: "#img1",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        x: 30,
+        ease: Power2,
+    })
+}
+function getp() {
+    gsap.to(".get-p", {
+        scrollTrigger: {
+            trigger: ".get-p",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function creativecontent() {
+    gsap.to("#creative-content", {
+        scrollTrigger: {
+            trigger: "#creative-content",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function getp2() {
+    gsap.to("#get-p2", {
+        scrollTrigger: {
+            trigger: "#get-p2",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function sandboximg2() {
+    gsap.to("#get-connect-img2", {
+        scrollTrigger: {
+            trigger: "#get-p2",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1,
+            duration: 5
+        },
+        y: -78,
+        ease: Power2,
+    })
+}
+function ytvideop() {
+    gsap.to("#yt-video-p", {
+        scrollTrigger: {
+            trigger: "#yt-video-p",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function approach() {
+    gsap.to("#approach", {
+        scrollTrigger: {
+            trigger: "#approach",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function brand() {
+    gsap.to("#brand h1", {
+        scrollTrigger: {
+            trigger: "#brand h1",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function brandp() {
+    gsap.to("#brand p", {
+        scrollTrigger: {
+            trigger: "#brand",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function brandbutton() {
+    gsap.to("#brand button", {
+        scrollTrigger: {
+            trigger: "#brand",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function digital() {
+    gsap.to(".digital p", {
+        scrollTrigger: {
+            trigger: ".digital p",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function digitalh1() {
+    gsap.to(".digital h1", {
+        scrollTrigger: {
+            trigger: ".digital h1",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function digitalp() {
+    gsap.to("#digital-p", {
+        scrollTrigger: {
+            trigger: "#digital-p",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function workh1() {
+    gsap.to(".work h1", {
+        scrollTrigger: {
+            trigger: ".work",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function play() {
+    gsap.to(".play p", {
+        scrollTrigger: {
+            trigger: ".play",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function playh1() {
+    gsap.to(".play h1", {
+        scrollTrigger: {
+            trigger: ".play",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+function branches() {
+    gsap.to("#branches h1", {
+        scrollTrigger: {
+            trigger: "#branches",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        opacity:1,
+        ease: Power2,
+    })
+}
+sandboximg();
+getp();
+getp2();
+creativecontent();
 loaderAnimation();
+sandboximg2();
+ytvideop();
+approach();
+brand();
+brandp();
+brandbutton();
+digital();
+digitalh1();
+digitalp();
+workh1();
+play();
+playh1();
+branches();
 animation();
 themechange();
 loco();
